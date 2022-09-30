4153b45f56135ecdbefe8ece9fa7dd7ac4cc0a61
AmamiyaRenn <379617892@qq.com>
2022-09-30 22:54:58 +0800

    完成了cachelab（未满分）

--- cachelab/csim.c
+++ cachelab/csim.c
@@ -1,7 +1,168 @@
 #include "cachelab.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <getopt.h>
+#include <stdbool.h>
+#include <memory.h>
 
-int main()
+typedef unsigned long long int uint;
+
+// print helpful diagnostic information
+static void usage(char *name)
 {
-    printSummary(0, 0, 0);
+    printf("Usage: %s [-hv] -s <num> -E <num> -b <num> -t <file>\n", name);
+    printf("Options:\n");
+    printf("  -h         Print this help message.\n");
+    printf("  -v         Optional verbose flag.\n");
+    printf("  -s <num>   Number of set index bits.\n");
+    printf("  -E <num>   Number of lines per set.\n");
+    printf("  -b <num>   Number of block offset bits.\n");
+    printf("  -t <file>  Trace file.\n");
+    exit(0);
+}
+
+typedef struct
+{
+    uint valid;
+    uint tag;
+    uint time_stamp;
+} cache_line;
+
+int main(int argc, char **argv)
+{
+    bool verbose = false;
+    uint s = 0;
+    uint E = 0;
+    uint b = 0;
+    char *trace_filepath = 0;
+
+    // 1. read arguments and initialize
+    int option = 0;
+    char argFlag = 0;
+    // read arguments
+    while ((option = getopt(argc, argv, "hvs:E:b:t:")) != -1)
+    {
+        switch (option)
+        {
+        case 'h':
+            usage(argv[0]);
+            break;
+
+        case 'v':
+            verbose = true;
+            break;
+
+        case 's':
+            s = atoi(optarg);
+            argFlag |= 0b1;
+            break;
+
+        case 'E':
+            E = atoi(optarg);
+            argFlag |= 0b10;
+            break;
+
+        case 'b':
+            b = atoi(optarg);
+            argFlag |= 0b100;
+            break;
+
+        case 't':
+            trace_filepath = optarg;
+            argFlag |= 0b1000;
+            break;
+
+        default:
+            printf("Invalid option '%c'\n", option);
+            usage(argv[0]);
+            break;
+        }
+    }
+    if (argFlag ^ 0b1111)
+    {
+        printf("%s: Missing required command line argument\n", argv[0]);
+        usage(argv[0]);
+    }
+    // initialize cache_line
+    cache_line cache[1 << s][E]; // cache[set][line]
+    memset(cache, 0, sizeof(cache));
+
+    // 2. read trace file and process
+    FILE *trace_file = fopen(trace_filepath, "r");
+    if (trace_file == NULL)
+    {
+        printf("%s: Open file failed!\n", argv[0]);
+        usage(argv[0]);
+    }
+
+    uint hits = 0, misses = 0, evictions = 0;
+    uint time_stamp = 0;
+    while (!feof(trace_file))
+    {
+        time_stamp++;
+
+        // get one command line
+        char op = 'x';
+        uint address, size;
+        char command_line[64];
+        memset(command_line, 0, sizeof(command_line));
+        fgets(command_line, sizeof(command_line), trace_file);
+        if (command_line[0] == 'I')
+            continue;
+        sscanf(command_line, " %c %llx,%lld", &op, &address, &size);
+        if (op == 'x')
+            break;
+
+        // process
+        if (verbose)
+            printf("%c %llx,%lld", op, address, size);
+        bool hit = false;
+        uint tag = address >> b;
+        uint set = tag % (1 << s);
+        for (uint isM = op == 'M' ? 2 : 1; isM != 0; isM--)
+        {
+            for (uint line = 0; line < E; line++)
+            {
+                if (cache[set][line].valid && tag == cache[set][line].tag)
+                {
+                    cache[set][line].time_stamp = time_stamp;
+                    hits++;
+                    hit = true;
+                    if (verbose)
+                        printf(" hit");
+                    break;
+                }
+            }
+            if (!hit)
+            {
+                misses++;
+                uint leastUsedLine = 0;
+                for (uint line = 1; line < E; line++)
+                    if (cache[set][line].time_stamp < cache[set][leastUsedLine].time_stamp)
+                        leastUsedLine = line;
+                if (cache[set][leastUsedLine].time_stamp)
+                {
+                    evictions++;
+                    cache[set][leastUsedLine].time_stamp = time_stamp;
+                    cache[set][leastUsedLine].tag = tag;
+                    if (verbose)
+                        printf(" miss eviciton");
+                }
+                else
+                {
+                    cache[set][leastUsedLine].time_stamp = time_stamp;
+                    cache[set][leastUsedLine].tag = tag;
+                    cache[set][leastUsedLine].valid = true;
+                    if (verbose)
+                        printf(" miss");
+                }
+            }
+        }
+        if (verbose)
+            printf("\n");
+    }
+
+    printSummary(hits, misses, evictions);
     return 0;
 }
--- cachelab/trans.c
+++ cachelab/trans.c
@@ -1,4 +1,4 @@
-/* 
+/*
  * trans.c - Matrix transpose B = A^T
  *
  * Each transpose function must have a prototype of the form:
@@ -6,44 +6,188 @@
  *
  * A transpose function is evaluated by counting the number of misses
  * on a 1KB direct mapped cache with a block size of 32 bytes.
- */ 
+ */
 #include <stdio.h>
 #include "cachelab.h"
 
 int is_transpose(int M, int N, int A[N][M], int B[M][N]);
 
-/* 
+/*
  * transpose_submit - This is the solution transpose function that you
  *     will be graded on for Part B of the assignment. Do not change
  *     the description string "Transpose submission", as the driver
  *     searches for that string to identify the transpose function to
- *     be graded. 
+ *     be graded.
  */
 char transpose_submit_desc[] = "Transpose submission";
 void transpose_submit(int M, int N, int A[N][M], int B[M][N])
 {
+    if (M == 32 && N == 32)
+    {
+        int tmp0, tmp1, tmp2, tmp3, tmp4, tmp5, tmp6, tmp7;
+        for (int row = 0; row < M; row += 8)
+            for (int col = 0; col < N; col += 8)
+                for (int i = row; i < row + 8; i++)
+                {
+                    tmp0 = A[i][col];
+                    tmp1 = A[i][col + 1];
+                    tmp2 = A[i][col + 2];
+                    tmp3 = A[i][col + 3];
+                    tmp4 = A[i][col + 4];
+                    tmp5 = A[i][col + 5];
+                    tmp6 = A[i][col + 6];
+                    tmp7 = A[i][col + 7];
+                    B[col][i] = tmp0;
+                    B[col + 1][i] = tmp1;
+                    B[col + 2][i] = tmp2;
+                    B[col + 3][i] = tmp3;
+                    B[col + 4][i] = tmp4;
+                    B[col + 5][i] = tmp5;
+                    B[col + 6][i] = tmp6;
+                    B[col + 7][i] = tmp7;
+                }
+    }
+    else if (M == 64 && N == 64)
+    {
+        int tmp0, tmp1, tmp2, tmp3;
+        for (int row = 0; row < N; row += 4)
+            for (int col = 0; col < M; col += 4)
+                for (int i = row; i < row + 4; i++)
+                {
+                    tmp0 = A[i][col];
+                    tmp1 = A[i][col + 1];
+                    tmp2 = A[i][col + 2];
+                    tmp3 = A[i][col + 3];
+                    B[col][i] = tmp0;
+                    B[col + 1][i] = tmp1;
+                    B[col + 2][i] = tmp2;
+                    B[col + 3][i] = tmp3;
+                }
+    }
+    else if (M == 61 && N == 67)
+    {
+        int tmp0, tmp1, tmp2, tmp3;
+        int row, col;
+        int rowMax = N / 4 * 4, colMax = M / 4 * 4;
+        for (row = 0; row < rowMax; row += 4)
+            for (col = 0; col < colMax; col += 4)
+                for (int i = row; i < row + 4; i++)
+                {
+                    tmp0 = A[i][col];
+                    tmp1 = A[i][col + 1];
+                    tmp2 = A[i][col + 2];
+                    tmp3 = A[i][col + 3];
+                    B[col][i] = tmp0;
+                    B[col + 1][i] = tmp1;
+                    B[col + 2][i] = tmp2;
+                    B[col + 3][i] = tmp3;
+                }
+        for (row = rowMax; row < N; row++)
+            for (col = 0; col < colMax; col++)
+            {
+                tmp0 = A[row][col];
+                B[col][row] = tmp0;
+            }
+        for (row = 0; row < rowMax; row++)
+            for (col = colMax; col < M; col++)
+            {
+                tmp0 = A[row][col];
+                B[col][row] = tmp0;
+            }
+        for (row = rowMax; row < N; row++)
+            for (col = colMax; col < M; col++)
+            {
+                tmp0 = A[row][col];
+                B[col][row] = tmp0;
+            }
+    }
 }
 
-/* 
+/*
  * You can define additional transpose functions below. We've defined
- * a simple one below to help you get started. 
- */ 
+ * a simple one below to help you get started.
+ */
+char transpose_submit_32_32_desc[] = "Transpose submission 32*32";
+void transpose_submit_32_32(int M, int N, int A[N][M], int B[M][N])
+{
+    for (int row = 0; row < N; row += 8)
+        for (int col = 0; col < M; col += 8)
+            for (int i = row; i < row + 8; i++)
+                for (int j = col; j < col + 8; j++)
+                    B[j][i] = A[i][j];
+}
 
-/* 
+char transpose_submit_64_64_desc[] = "Transpose submission 64*64";
+void transpose_submit_64_64(int M, int N, int A[N][M], int B[M][N])
+{
+    int tmp0, tmp1, tmp2, tmp3;
+    for (int row = 0; row < N; row += 4)
+        for (int col = 0; col < M; col += 4)
+            for (int i = row; i < row + 4; i++)
+            {
+                tmp0 = A[i][col];
+                tmp1 = A[i][col + 1];
+                tmp2 = A[i][col + 2];
+                tmp3 = A[i][col + 3];
+                B[col][i] = tmp0;
+                B[col + 1][i] = tmp1;
+                B[col + 2][i] = tmp2;
+                B[col + 3][i] = tmp3;
+            }
+}
+
+char transpose_submit_61_67_desc[] = "Transpose submission 61*67";
+void transpose_submit_61_67(int M, int N, int A[N][M], int B[M][N])
+{
+    int tmp0, tmp1, tmp2, tmp3;
+    int row, col;
+    int rowMax = N / 4 * 4, colMax = M / 4 * 4;
+    for (row = 0; row < rowMax; row += 4)
+        for (col = 0; col < colMax; col += 4)
+            for (int i = row; i < row + 4; i++)
+            {
+                tmp0 = A[i][col];
+                tmp1 = A[i][col + 1];
+                tmp2 = A[i][col + 2];
+                tmp3 = A[i][col + 3];
+                B[col][i] = tmp0;
+                B[col + 1][i] = tmp1;
+                B[col + 2][i] = tmp2;
+                B[col + 3][i] = tmp3;
+            }
+    for (row = rowMax; row < N; row++)
+        for (col = 0; col < colMax; col++)
+        {
+            tmp0 = A[row][col];
+            B[col][row] = tmp0;
+        }
+    for (row = 0; row < rowMax; row++)
+        for (col = colMax; col < M; col++)
+        {
+            tmp0 = A[row][col];
+            B[col][row] = tmp0;
+        }
+    for (row = rowMax; row < N; row++)
+        for (col = colMax; col < M; col++)
+        {
+            tmp0 = A[row][col];
+            B[col][row] = tmp0;
+        }
+}
+
+/*
  * trans - A simple baseline transpose function, not optimized for the cache.
  */
 char trans_desc[] = "Simple row-wise scan transpose";
 void trans(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j, tmp;
-
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; j++) {
+    for (i = 0; i < N; i++)
+        for (j = 0; j < M; j++)
+        {
             tmp = A[i][j];
             B[j][i] = tmp;
         }
-    }    
-
 }
 
 /*
@@ -56,14 +200,15 @@ void trans(int M, int N, int A[N][M], int B[M][N])
 void registerFunctions()
 {
     /* Register your solution function */
-    registerTransFunction(transpose_submit, transpose_submit_desc); 
+    registerTransFunction(transpose_submit, transpose_submit_desc);
+
+    // registerTransFunction(transpose_submit_64_64, transpose_submit_64_64_desc);
 
     /* Register any additional transpose functions */
-    registerTransFunction(trans, trans_desc); 
-
+    // registerTransFunction(trans, trans_desc);
 }
 
-/* 
+/*
  * is_transpose - This helper function checks if B is the transpose of
  *     A. You can check the correctness of your transpose by calling
  *     it before returning from the transpose function.
@@ -72,13 +217,15 @@ int is_transpose(int M, int N, int A[N][M], int B[M][N])
 {
     int i, j;
 
-    for (i = 0; i < N; i++) {
-        for (j = 0; j < M; ++j) {
-            if (A[i][j] != B[j][i]) {
+    for (i = 0; i < N; i++)
+    {
+        for (j = 0; j < M; ++j)
+        {
+            if (A[i][j] != B[j][i])
+            {
                 return 0;
             }
         }
     }
     return 1;
 }
-
